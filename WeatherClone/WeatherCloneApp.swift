854b1fe61325da46d210439b36aa8187524f4630
Tuyenkaka2005 <nguyendangtuyen2005@gmail.com>
2026-05-13 00:04:03 -0700

    Initial Commit

---
+++ WeatherClone/WeatherCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherCloneApp.swift
+//  WeatherClone
+//
+//  Created by Tuyen on 13/5/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
